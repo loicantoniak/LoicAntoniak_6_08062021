f5bd15ad859f0a2b15909d35a87209b8eea5e50d
Antoniak Loïc <loic.antoniak@gmail.com>
2021-09-10 10:24:27 +0200

    fix data.json

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -1,11 +1,7 @@
 import { mainPage } from "./index.js";
 import { photographerPage } from "./photographer.js";
 
-<<<<<<< HEAD
 const url = "../data.json";
-=======
-const url = "../../data.json";
->>>>>>> 50c3ba694e3fafeb1fde18a9a8bb612eb36fdcac
 
 export const fetchData = async function () {
   try {
